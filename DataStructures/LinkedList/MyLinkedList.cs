756fb3f3ba5f62f804dcc053c56e767c953c090a
Ihor Pavlov <Ihor.Pavlov@outlook.com>
2025-02-24 21:25:29 +0200

    Add custom linked list implementation
    Updated target framework in `DataStructures.csproj` to `net9.0` and removed unnecessary `<ItemGroup>` sections. Replaced initial template code in `Program.cs` with a new implementation using `MyLinkedList<T>`. Added `MyLinkedList<T>` class in `MyLinkedList.cs` to implement a singly linked list with methods for adding, removing, and checking elements. Added `Node<T>` class in `Node.cs` to represent a node in the linked list.

---
+++ DataStructures/LinkedList/MyLinkedList.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.LinkedList
+{
+    internal class MyLinkedList<T> : IEnumerable<T>
+    {
+        public Node<T> Head { get; set; }
+        public Node<T> Tail { get; set; }
+        public int Count { get; private set; }
+        public MyLinkedList() => Clear();
+        public MyLinkedList(T data)
+        {
+            SetHeadAndTail(data);
+        }
+
+        private void SetHeadAndTail(T data)
+        {
+            Head = new Node<T>(data);
+            Tail = Head;
+            Count++;
+        }
+
+        public void AppendFirst(T data)
+        {
+            var newHead = new Node<T>(data);
+            newHead.Next = Head;
+            Head = newHead;
+            Count++;
+        }
+        public void Add(T data)
+        {
+            if (Head is null) SetHeadAndTail(data);
+            else
+            {
+                Tail.Next = new Node<T>(data);
+                Tail = Tail.Next;
+                Count++;
+            }
+        }
+        public void Remove(T data)
+        {
+            if (Head is null) return;
+            if (Head.Data.Equals(data))
+            {
+                Head = Head.Next;
+                Count--;
+                return;
+            }
+
+            var previous = Head;
+            var current = Head.Next;
+            while (current is not null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    previous.Next = current.Next;
+                    Count--;
+                    return;
+                }
+                previous = current;
+                current = current.Next;
+            }
+        }
+        public void Clear()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+        public bool Contains(T data)
+        {
+            var current = Head;
+            while (current is not null)
+            {   
+                if (current.Data.Equals(data)) return true;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = Head;
+            while (current is not null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataStructures.LinkedList;
+
+Console.WriteLine("Start is here");
+
+
+
+var myLinkedList = new MyLinkedList<int>();
+myLinkedList.Add(1);
+myLinkedList.Add(2);
+myLinkedList.Add(3);
+myLinkedList.Add(4);
+myLinkedList.Add(5);
+
+foreach (var item in myLinkedList)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
+
+myLinkedList.Remove(1);
+myLinkedList.Remove(3);
+myLinkedList.Remove(5);
+myLinkedList.Remove(7);
+myLinkedList.AppendFirst(0);
+
+foreach (var item in myLinkedList)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
+
+Console.WriteLine(myLinkedList.Contains(0));
+Console.WriteLine(myLinkedList.Contains(7));
+
+
+Console.ReadLine();
