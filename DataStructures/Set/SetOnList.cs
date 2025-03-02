15f63c1e788a4a1afd9d693c3dabf4c9d484f21e
Ihor Pavlov <Ihor.Pavlov@outlook.com>
2025-03-02 15:32:10 +0200

    Implement set operations and related tests
    Refactor `Program.cs` to replace queue implementation with set operations, introducing `SetOnList<T>` for managing sets. Update `SimpleTests.csproj` to include the new `Set` namespace. Add `SetOnListTests.cs` for unit testing the set functionalities, ensuring correctness of operations like union, intersection, and difference.

---
+++ DataStructures/Set/SetOnList.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+
+namespace DataStructures.Set
+{
+    public class SetOnList<T> : IEnumerable<T>, ICollection
+    {
+        private readonly List<T> _items = new();
+        public int Count => _items.Count;
+        public bool IsEmpty => Count == 0;
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public bool IsSynchronized => throw new NotImplementedException();
+
+        public object SyncRoot => throw new NotImplementedException();
+
+        public SetOnList() => Clear();
+        public SetOnList(T item) => Add(item);
+        public SetOnList(IEnumerable<T> collection)
+        {
+            foreach (var item in collection) Add(item);
+        }
+
+        public void Add(T item)
+        {
+            if (_items.Contains(item)) return;
+            _items.Add(item);
+        }
+        public void Remove(T item) => _items.Remove(item);
+        public void Clear() => _items.Clear();
+        public SetOnList<T> UnionWith(SetOnList<T> other)
+        {
+            var result = new SetOnList<T>(_items);
+            foreach (var item in other) result.Add(item);
+            return result;
+
+            //LINQ Alternative:
+            //return new SetOnList<T>(this.Union(other));
+        }
+        public SetOnList<T> IntersectWith(SetOnList<T> other)
+        {
+            var result = new SetOnList<T>();
+            foreach (var item in _items)
+            {
+                if (other.Contains(item)) result.Add(item);
+            }
+            return result;
+
+            //LINQ Alternative:
+            //return new SetOnList<T>(this.Intersect(other));
+        }
+        public SetOnList<T> DifferenceWith(SetOnList<T> other)
+        {
+            var result = new SetOnList<T>(_items);
+            foreach (var item in other) result.Remove(item);
+            return result;
+
+            //LINQ Alternative:
+            //return new SetOnList<T>(this.Except(other));
+        }
+        public bool IsSubset(SetOnList<T> other)
+        {
+            foreach (var item in _items)
+            {
+                if (!other.Contains(item)) return false;
+            }
+            return true;
+
+            //LINQ Alternative:
+            //return this.All(other.Contains);
+        }
+        public SetOnList<T> SymmetricDifferenceWith(SetOnList<T> other)
+        {
+            var result = new SetOnList<T>(_items);
+            foreach (var item in other)
+            {
+                if (result.Contains(item)) result.Remove(item);
+                else result.Add(item);
+            }
+            return result;
+
+            //LINQ Alternative:
+            //return new SetOnList<T>(this.Except(other).Union(other.Except(this)));
+        }
+
+        public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public void CopyTo(Array array, int index) => _items.CopyTo((T[])array, index);
+    }
+}
---
+++ SimpleTests/Set/SetOnListTests.cs
@@ -0,0 +1,108 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataStructures.Set;
+using System.Collections.Generic;
+
+namespace DataStructures.Tests.Set
+{
+    [TestClass]
+    public class SetOnListTests
+    {
+        [TestMethod]
+        public void TestConstructor_Empty()
+        {
+            var set = new SetOnList<int>();
+            Assert.AreEqual(0, set.Count);
+            Assert.IsTrue(set.IsEmpty);
+        }
+
+        [TestMethod]
+        public void TestConstructor_SingleItem()
+        {
+            var set = new SetOnList<int>(5);
+            Assert.AreEqual(1, set.Count);
+            Assert.IsFalse(set.IsEmpty);
+        }
+
+        [TestMethod]
+        public void TestConstructor_Collection()
+        {
+            var set = new SetOnList<int>(new[] { 1, 2, 3 });
+            Assert.AreEqual(3, set.Count);
+            Assert.IsFalse(set.IsEmpty);
+        }
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            var set = new SetOnList<int>();
+            set.Add(1);
+            Assert.AreEqual(1, set.Count);
+            set.Add(1); // Adding duplicate
+            Assert.AreEqual(1, set.Count);
+        }
+
+        [TestMethod]
+        public void TestRemove()
+        {
+            var set = new SetOnList<int>(new[] { 1, 2, 3 });
+            set.Remove(2);
+            Assert.AreEqual(2, set.Count);
+            set.Remove(4); // Removing non-existent item
+            Assert.AreEqual(2, set.Count);
+        }
+
+        [TestMethod]
+        public void TestClear()
+        {
+            var set = new SetOnList<int>(new[] { 1, 2, 3 });
+            set.Clear();
+            Assert.AreEqual(0, set.Count);
+            Assert.IsTrue(set.IsEmpty);
+        }
+
+        [TestMethod]
+        public void TestUnionWith()
+        {
+            var setA = new SetOnList<int>(new[] { 1, 2, 3 });
+            var setB = new SetOnList<int>(new[] { 3, 4, 5 });
+            var result = setA.UnionWith(setB);
+            CollectionAssert.AreEquivalent(new[] { 1, 2, 3, 4, 5 }, result);
+        }
+
+        [TestMethod]
+        public void TestIntersectWith()
+        {
+            var setA = new SetOnList<int>(new[] { 1, 2, 3 });
+            var setB = new SetOnList<int>(new[] { 3, 4, 5 });
+            var result = setA.IntersectWith(setB);
+            CollectionAssert.AreEquivalent(new[] { 3 }, result);
+        }
+
+        [TestMethod]
+        public void TestDifferenceWith()
+        {
+            var setA = new SetOnList<int>(new[] { 1, 2, 3 });
+            var setB = new SetOnList<int>(new[] { 3, 4, 5 });
+            var result = setA.DifferenceWith(setB);
+            CollectionAssert.AreEquivalent(new[] { 1, 2 }, result);
+        }
+
+        [TestMethod]
+        public void TestIsSubset()
+        {
+            var setA = new SetOnList<int>(new[] { 1, 2, 3 });
+            var setB = new SetOnList<int>(new[] { 1, 2, 3, 4, 5 });
+            Assert.IsTrue(setA.IsSubset(setB));
+            Assert.IsFalse(setB.IsSubset(setA));
+        }
+
+        [TestMethod]
+        public void TestSymmetricDifferenceWith()
+        {
+            var setA = new SetOnList<int>(new[] { 1, 2, 3 });
+            var setB = new SetOnList<int>(new[] { 3, 4, 5 });
+            var result = setA.SymmetricDifferenceWith(setB);
+            CollectionAssert.AreEquivalent(new[] { 1, 2, 4, 5 }, result);
+        }
+    }
+}
